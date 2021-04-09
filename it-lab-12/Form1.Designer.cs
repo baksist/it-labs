4206d12f2f5673b7dc1a17ea3cac0d1ffbdbde95
matt <matvey.serdukov@gmail.com>
2021-04-09 16:33:17 +0300

    lab 12 finished

--- it-lab-12/Form1.Designer.cs
+++ it-lab-12/Form1.Designer.cs
@@ -29,9 +29,9 @@ namespace it_lab_12
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.arg = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.func = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -39,22 +39,11 @@ namespace it_lab_12
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.button5 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // chart1
-            // 
-            chartArea5.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea5);
-            legend5.Name = "Legend1";
-            this.chart1.Legends.Add(legend5);
-            this.chart1.Location = new System.Drawing.Point(22, 22);
-            this.chart1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.chart1.Name = "chart1";
-            this.chart1.Size = new System.Drawing.Size(423, 236);
-            this.chart1.TabIndex = 0;
-            this.chart1.Text = "chart1";
             // 
             // dataGridView1
             // 
@@ -63,12 +52,13 @@ namespace it_lab_12
             this.arg,
             this.func});
             this.dataGridView1.Location = new System.Drawing.Point(466, 22);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(314, 236);
             this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
             // 
             // arg
             // 
@@ -87,7 +77,7 @@ namespace it_lab_12
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(22, 299);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(152, 58);
             this.button1.TabIndex = 2;
@@ -98,7 +88,7 @@ namespace it_lab_12
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(216, 299);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(145, 58);
             this.button2.TabIndex = 3;
@@ -109,7 +99,7 @@ namespace it_lab_12
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(647, 263);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(56, 19);
             this.button3.TabIndex = 4;
@@ -124,33 +114,59 @@ namespace it_lab_12
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(145, 58);
             this.button4.TabIndex = 5;
-            this.button4.Text = "2nd order";
+            this.button4.Text = "Most precise";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(596, 299);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(145, 58);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Graph";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(22, 22);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(401, 236);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(840, 441);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.chart1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridViewTextBoxColumn arg;
         private System.Windows.Forms.DataGridViewTextBoxColumn func;
@@ -158,6 +174,8 @@ namespace it_lab_12
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- it-lab-12/Form1.cs
+++ it-lab-12/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace it_lab_12
 {
@@ -34,7 +35,10 @@ namespace it_lab_12
                 points.Add(new Tuple<double, double>(x, y));
             }
 
-            Console.WriteLine(LinearRegression.GetPrecisePolynomial(1));
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -47,6 +51,19 @@ namespace it_lab_12
         {
             var a = LinearRegression.GetPolynomial(2);
             MessageBox.Show($"{a[0]}+{a[1]}x+{a[2]}x^2", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var order = LinearRegression.GetPrecisePolynomial(1);
+            var a = LinearRegression.GetPolynomial(order);
+            var res = "";
+            for (var i = 0; i < a.Count; i++)
+            {
+                res += $"{a[i]} * x^{i} + ";
+            }
+            MessageBox.Show($"{res}", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
         }
 
         private void fillTable()
@@ -69,17 +86,79 @@ namespace it_lab_12
             dataGridView1.Rows[5].Cells[1].Value = 3.0;
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void button5_Click(object sender, EventArgs e)
         {
-            var order = LinearRegression.GetPrecisePolynomial(1);
-            var a = LinearRegression.GetPolynomial(order);
-            var res = "";
-            for (var i = 0; i < a.Count; i++)
-            {
-                res += $"{a[i]} * x^{i} + ";
-            }
-            MessageBox.Show($"{res}", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            double[] x = points.Select(_ => _.Item1).ToArray();
+            double[] y = points.Select(_ => _.Item2).ToArray();
+           
+            double Xmin = x.Min();
+            double Xmax = x.Max();
+            double Ymin = y.Min();
+            double Ymax = y.Max();
+            double step_x = (Xmax - Xmin) / x.Length;
+            double step_y = (Ymax - Ymin) / y.Length;
 
+            double[] y_1 = LinearRegression.CalculateY(1);
+            double[] y_2 = LinearRegression.CalculateY(2);
+            double[] y_p = LinearRegression.CalculateY(LinearRegression.GetPrecisePolynomial(1));
+
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+            chart1.Legends.Clear();
+
+            ChartArea area = new ChartArea();
+            area.Name = "myGraph";
+            area.AxisX.Minimum = Xmin;
+            area.AxisY.Maximum = Xmax;
+            area.AxisX.MajorGrid.Interval = step_x;
+            area.AxisY.Minimum = Ymin;
+            area.AxisY.Maximum = Ymax;
+            area.AxisY.MajorGrid.Interval = step_y;
+            chart1.ChartAreas.Add(area);
+
+            Series series_0 = new Series();
+            series_0.ChartArea = "myGraph";
+            series_0.ChartType = SeriesChartType.Point;
+            series_0.MarkerSize = 10;
+            series_0.LegendText = "p(x)_0";
+            chart1.Series.Add(series_0);
+
+            Series series_1 = new Series();
+            series_1.ChartArea = "myGraph";
+            series_1.ChartType = SeriesChartType.Spline;
+            series_1.BorderWidth = 3;
+            series_1.LegendText = "p(x)_1";
+            chart1.Series.Add(series_1);
+
+            Series series_2 = new Series();
+            series_2.ChartArea = "myGraph";
+            series_2.ChartType = SeriesChartType.Spline;
+            series_2.BorderWidth = 3;
+            series_2.LegendText = "p(x)_2";
+            chart1.Series.Add(series_2);
+
+            Series series_p = new Series();
+            series_p.ChartArea = "myGraph";
+            series_p.ChartType = SeriesChartType.Spline;
+            series_p.BorderWidth = 3;
+            series_p.LegendText = "p(x)_precise";
+            chart1.Series.Add(series_p);
+
+            Legend legend = new Legend();
+            chart1.Legends.Add(legend);
+
+            chart1.Series[0].Points.DataBindXY(x, y);
+            chart1.Series[1].Points.DataBindXY(x, y_1);
+            chart1.Series[2].Points.DataBindXY(x, y_2);
+            chart1.Series[3].Points.DataBindXY(x, y_p);
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
         }
     }
 }
